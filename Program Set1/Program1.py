6b054615ea790949539e0dcce34b8f711e1f36c2
suleman-814 <suleman0814@gmail.com>
2025-10-17 09:31:23 +0530

    Initial commit

---
+++ Program Set1/Program1.py
@@ -0,0 +1,12 @@
+# Program to perform different Arithmetic Operations on numbers.
+
+#By Assigning input :
+
+a = 100
+b = 5
+print('Sum: ', a + b)
+print('Subtraction: ', a - b)
+print('Multiplication: ', a * b)
+print('Division: ', a / b)
+print('Modulo: ', a % b)
+print('Power: ', a ** b)
