0a57cb9764e6b372a64990d5e1587348ddc74f32
Suleman-814 <suleman0814@gmail.com>
2025-10-18 11:50:49 +0530

    Add files via upload

---
+++ Program Set1/Program2.py
@@ -0,0 +1,25 @@
+#Program to perform different Arithmetic Operations on numbers.
+
+# By Assigning input :
+
+a = 100
+b = 5
+print('Sum: ', a + b)
+print('Subtraction: ', a - b)
+print('Multiplication: ', a * b)
+print('Division: ', a / b)
+print('Modulo: ', a % b)
+print('Power: ', a ** b)
+
+###########################################################################################################################
+
+# By taking input from  user :
+
+num1 = int(input("Enter Your 1st Number"))
+num2 = int(input("Enter Your 2nd Number"))
+print('Sum: ', num1 + num2)
+print('Subtraction: ', num1 - num2)
+print('Multiplication: ', num1 * num2)
+print('Division: ', num1 / num2)
+print('Modulo: ', num1 % num2)
+print('Power: ', num1 ** num2)
