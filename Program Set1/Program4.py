d5d06203a79ca4426fc77a2f138991a80f330008
Suleman-814 <suleman0814@gmail.com>
2025-10-20 16:20:37 +0530

    Add files via upload

---
+++ Program Set1/Program4.py
@@ -0,0 +1,17 @@
+#Program to create, concatenate and print a string and accessing sub-string from a given string
+
+#create a string
+string1= "Google Chrome"
+string2= "Microsoft Edge"
+string3= "Firefox"
+string4= "Safari"
+
+#concatenate a string
+string= string1+" " +string2+" " +string3+" " +string4
+
+#print a string
+print(string)
+
+#accessing a substring
+substring= string[3:17]
+print(substring)
