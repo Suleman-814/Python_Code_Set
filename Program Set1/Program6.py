5ae7e8ca71c768047bde77c3fb60e78b4391df54
Suleman-814 <suleman0814@gmail.com>
2025-10-22 13:28:58 +0530

    Add files via upload

---
+++ Program Set1/Program6.py
@@ -0,0 +1,15 @@
+#Program to create, append, and remove lists in python
+
+#Create a List
+List1=["C","C++","C#","Python","Java","Javascript"]
+
+#print a list
+print(List1)
+
+#Append a list
+List1.append("Java")
+print(List1)
+
+#Remove from List
+List1.remove("C#")
+print(List1)
