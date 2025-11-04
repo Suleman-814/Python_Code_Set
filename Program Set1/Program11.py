dd1333b20b6b2bb7895fb043cb675c69709935d1
Suleman-814 <suleman0814@gmail.com>
2025-11-04 18:40:33 +0530

    Add files via upload

---
+++ Program Set1/Program11.py
@@ -0,0 +1,13 @@
+# Python program to  prints prime numbers less than 100
+
+def is_prime(n):
+    if n <= 1:
+        return False
+    for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+            return False
+    return True
+
+for num in range(100):
+    if is_prime(num):
+        print(num)
