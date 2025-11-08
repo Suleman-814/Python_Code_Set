d7cead6480ca851a98aa7d9170db46f23f5b7cd3
Suleman-814 <suleman0814@gmail.com>
2025-11-08 17:48:48 +0530

    Add files via upload

---
+++ Program Set2/Program2.py
@@ -0,0 +1,9 @@
+# By taking input from  user :
+num1 = int(input("Enter Your 1st Number"))
+num2 = int(input("Enter Your 2nd Number"))
+print('Sum: ', num1 + num2)
+print('Subtraction: ', num1 - num2)
+print('Multiplication: ', num1 * num2)
+print('Division: ', num1 / num2)
+print('Modulo: ', num1 % num2)
+print('Power: ', num1 ** num2)
