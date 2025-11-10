c5fa285ca3dc3ebdeafed5e59623b899f2a323e1
Suleman-814 <suleman0814@gmail.com>
2025-11-10 20:04:34 +0530

    Add files via upload

---
+++ Program Set2/Program4.py
@@ -0,0 +1,8 @@
+#WAP to find  even or odd number by taking input from user?
+
+check_num= int(input("Enter a Number for verifying it is odd or even : "))
+if check_num%2==0:
+    print(f"{check_num} is Even Number")
+else:
+    print(f"{check_num} is Odd Number")
+print("Thank You!")    
