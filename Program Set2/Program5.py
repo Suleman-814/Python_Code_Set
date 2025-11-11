19aba48456ce1a26f3966c93d01b1ba4b3a78645
Suleman-814 <suleman0814@gmail.com>
2025-11-11 18:47:15 +0530

    Add files via upload

---
+++ Program Set2/Program5.py
@@ -0,0 +1,8 @@
+#WAP to find user number is divisible by 3 or not
+
+divNum=int(input("Enter a Number: "))
+if divNum%3==0:
+    print(f"{divNum} is divisible by 3")
+else:
+    print(f"{divNum} is not divisible by 3")
+print("Thank You!")    
