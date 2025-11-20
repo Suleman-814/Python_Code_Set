d9529cf1f1d938e262912a001c710181540d54b5
Suleman-814 <suleman0814@gmail.com>
2025-11-20 19:39:07 +0530

    Add files via upload

---
+++ Program Set2/Program9.py
@@ -0,0 +1,39 @@
+# WAP to enter state to get a famous palces of that state
+
+c_list=["Delhi","Mumbai","Jaipur","Amritsar","Agra","Hyderabad","Sanchi","Madurai","Gujrat","Mysore"]
+print(c_list)
+
+city= str(input("Enter the City Name : "))
+if city=="Delhi":
+    Monuments= "Red Fort","QutabMinar","India Gate"
+    print("These are the some famous monuments in Delhi :", Monuments)
+if city=="Mumbai":
+    Monuments="Gateway of India"
+    print("These are the some famous monuments in Mumbai :", Monuments)
+if city=="Jaipur":
+    Monuments="HawaMahal"
+    print("These are the some famous monuents in Jaipur :", Monuments)
+if city=="Amritsar":
+    Monuments="Golden Temple","Jalliawala Bagh"
+    print("These are the some famous mnuments in Amritsar :", Monuments)
+if city=="Agra":
+    Monuments="Taj Mahal"
+    print("These are the some famous monuments in Agra :", Monuments)
+if city=="Hyderabad":
+    Monuments="Charminar"
+    print("These are the some famous monuments in Hyderabad :", Monuments)
+if city=="Sanchi":
+    Monuments="Sanchi Palace"
+    print("These are the some famous monuments in Sanchi :", Monuments)
+if city=="Madurai":
+    Monuments="Meenakshi Temple"
+    print("These are the some famous monuments in Mumbai :", Monuments)
+if city=="Gujrat":
+    Monuments=="Statue of unity"
+    print("These are the some famous monuments in Gujrat :", Monuments)
+if city=="Mysore":
+    Monuments="Mysore Palace"
+    print("These are the some famous monuments in Mysore :", Monuments)
+               
+                                        
+    
