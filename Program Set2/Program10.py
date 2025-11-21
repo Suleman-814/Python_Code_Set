50c83b77dc97767d7cf166ba76deee32b8762908
Suleman-814 <suleman0814@gmail.com>
2025-11-21 10:21:37 +0530

    Add files via upload

---
+++ Program Set2/Program10.py
@@ -0,0 +1,17 @@
+
+p= int(input("Enter the price of item : "))
+if p<0:
+    print("no negative")
+    if p>2000:
+        discount= 0.2
+        final_price = p - (discount * p)
+        print("Final price : ",final_price)
+    elif p>=1000:
+        discount= 0.1
+        final_price = p - (discount * p)
+        print("Final price : ",final_price)
+    elif p<1000:
+        amount=1000-p
+        print(f"you need to purchase ${amount} more ", amount)
+    else:
+        print("no disscount")
