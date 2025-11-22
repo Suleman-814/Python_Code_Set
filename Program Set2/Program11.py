d0d404780f08430ea3bb37439a419ec4ef01f024
Suleman-814 <suleman0814@gmail.com>
2025-11-22 10:03:43 +0530

    Add files via upload

---
+++ Program Set2/Program11.py
@@ -0,0 +1,23 @@
+
+ser= float(input("Enter the time period of service :"))
+sal= float(input("Enter the salary: "))
+if ser>10:
+    b=0.1
+    bn_Sal = sal + (b * sal)
+    net_b= sal*b
+    print(" bonus amount is: ", net_b)
+    print("Final Salary : ",bn_Sal)
+elif ser>=6 and ser<=10:
+    b=0.08
+    bn_Sal = sal + (b * sal)
+    net_b= sal*b
+    print(" bonus amount is: ", net_b)
+    print("Final Salary : ",bn_Sal)
+elif ser<6:
+    b=0.05
+    bn_Sal = sal + (b * sal)
+    net_b= sal*b
+    print(" bonus amount is: ", net_b)
+    print("Final Salary : ",bn_Sal)
+else:
+    print("Success only comes to those who work hard")
