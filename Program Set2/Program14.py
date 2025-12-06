5ec88caac448889c06eee66c554c5ed53aaea28e
Suleman-814 <suleman0814@gmail.com>
2025-12-06 19:49:46 +0530

    Add files via upload

---
+++ Program Set2/Program14.py
@@ -0,0 +1,7 @@
+number= float(input("Enter anumber"))
+if number==0:
+    print("0 is neither a positive nor negative number")
+elif number<0:
+    print("It is a negative number")
+elif number>0:
+    print("It is a positive number")
