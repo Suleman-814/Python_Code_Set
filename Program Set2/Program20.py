e25a3d084a3bbf2d0cae4ac6b89c05cfd6d55a41
Suleman-814 <suleman0814@gmail.com>
2025-12-20 21:30:13 +0530

    Add files via upload

---
+++ Program Set2/Program20.py
@@ -0,0 +1,13 @@
+# WAP to find even number till 100 using foor loop and while loop.
+
+for i in range(0,101):
+    if i%2==0:
+        print(i, end="\t")
+
+print("#############################################################")    
+i=0
+while i<101:
+    if i%2==0:
+        print(i, end="\t")
+    i+=1
+    
