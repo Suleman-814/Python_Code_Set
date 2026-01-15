caf44a118cf8d6767dd35a3789c2cd9022890eb8
Suleman-814 <suleman0814@gmail.com>
2026-01-15 21:37:32 +0530

    Add files via upload

---
+++ Program Set2/Program34.py
@@ -0,0 +1,12 @@
+##################Functions(Programs)##########
+
+#WAP To calculate the area, cicumfrence of circle using functions?
+
+def circle():
+    
+    Radius= float(input("Enter a number"))
+    Area = 3.14*Radius**2
+    Circumference=2*3.14*Radius
+    print( "Area of a circle is", Area)
+    print("Circomference of circle is:",Circumference)
+circle()    
