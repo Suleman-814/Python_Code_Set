c0651df3d415a976d35eeb6971f92934a0b4d63c
Suleman-814 <suleman0814@gmail.com>
2026-01-30 21:08:27 +0530

    Add files via upload

---
+++ Program Set2/Program43.py
@@ -0,0 +1,15 @@
+#WAP to Read Numbers add to list and print list when Zero Input entered.
+
+numbers = []
+
+while True:
+    num = int(input("Enter a number: "))
+
+    if num == 0:
+        print("0 is not allowed. Program stopped.")
+        break
+    else:
+        numbers.append(num)
+        print("Number added:", num)
+
+print("Final list:", numbers)
