d0b882a3e4dc814f2c7d83accb72aeca258a5b2d
Suleman-814 <suleman0814@gmail.com>
2026-02-02 11:50:32 +0530

    Add files via upload

---
+++ Program Set2/Program46.py
@@ -0,0 +1,10 @@
+#WAP to Accept Integer Input Using Exception Handling in a Loop
+
+while True:
+    try:
+        h = int(input("Enter a number: "))
+        print(h)
+        continue
+    except ValueError:
+        print("Some error! Please enter a valid number.")
+        break
