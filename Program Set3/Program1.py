fa408e3d9d9a5552aff098f200d6e636764541f6
Suleman-814 <suleman0814@gmail.com>
2026-02-05 18:43:42 +0530

    Add files via upload

--- Program Set3/Program1.py
+++ Program Set3/Program1.py
@@ -1,1 +1,19 @@
+# WAP to calculate the area and perimeter of a rectangle using a class
 
+
+class rectangle:
+    def __init__ (self):
+        a= float(input("Enter length of rectangle"))
+        b= float(input("Enter breadth of rectangle"))
+        self.a = a
+        self.b = b
+
+    def areaofrec(self):
+        area = (self.a*self.b)
+        print(f"area of {self.a} and {self.b} is:",area,"cm")
+    def per(self):
+        perimeter= 2*(self.a+self.b)
+        print(f"Perimeter of {self.a} and {self.b} is:",perimeter)
+x= rectangle()
+x.areaofrec()
+x.per()
