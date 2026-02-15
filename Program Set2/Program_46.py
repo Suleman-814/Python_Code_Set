c436b1a118165bbe3793ef683061bf3265f35815
Suleman-814 <suleman0814@gmail.com>
2026-02-15 17:56:04 +0530

    Update Program_46.py

--- Program Set2/Program_46.py
+++ Program Set2/Program_46.py
@@ -2,9 +2,10 @@
 
 while True:
     try:
-        h = int(input("Enter a number: "))
+        h = int(input("Enter a valid number: "))
         print(h)
         continue
     except ValueError:
         print("Some error! Please enter a valid number.")
         break
+
