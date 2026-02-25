f61ff416e89a8260650d33391b0a3daf612a0fe9
Suleman-814 <suleman0814@gmail.com>
2026-02-25 10:21:16 +0530

    Update Program_18.py

--- Program Set1/Program_18.py
+++ Program Set1/Program_18.py
@@ -1,9 +1,10 @@
 #WAP to Define a Function to Add Two Numbers and Return the Result.
 
 def func():
-    x1= float(input(" 1st number"))
-    x2= float(input(" 2nd number"))
+    x1= float(input("1st number"))
+    x2= float(input("2nd number"))
     sum = x1+x2
     return f"The sum of {x1} and {x2} is :{sum}"
 func()
     
+
