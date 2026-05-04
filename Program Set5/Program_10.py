8afbac1da52a67c706c6842b9a15636da5a65da2
Suleman-814 <suleman0814@gmail.com>
2026-05-04 21:20:35 +0530

    Update Program_10.py

--- Program Set5/Program_10.py
+++ Program Set5/Program_10.py
@@ -1,6 +1,6 @@
-n= (1,2,3,4,5,6,7,8,9,10,11,12,13,14,15)
+numbers= (1,2,3,4,5,6,7,8,9,10,11,12,13,14,15)
 from functools import reduce
 def sub(a,b):
     return a-b
-Result= reduce(sub, n)
+Result= reduce(sub, numbers)
 print(Result)
